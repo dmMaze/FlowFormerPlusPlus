0aa7559c704a115ccdb5ecaf5e1408b08c5140d9
dmMaze <beneathlimbo@gmail.com>
2024-04-05 00:25:27 +0800

    fix imports

--- core/FlowFormer/PerCostFormer3/encoder.py
+++ core/FlowFormer/PerCostFormer3/encoder.py
@@ -15,7 +15,7 @@ from typing import Optional, Tuple
 from .twins import Size_, PosConv
 from .cnn import TwinsSelfAttentionLayer, TwinsCrossAttentionLayer, BasicEncoder
 
-from timm.models.layers import Mlp, DropPath, activations, to_2tuple, trunc_normal_
+from timm.models.layers import DropPath
 
 class PatchEmbed(nn.Module):
     def __init__(self, patch_size=16, in_chans=1, embed_dim=64, pe='linear', cfg=None):
